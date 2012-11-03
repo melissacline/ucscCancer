6f968f697c69e23de7488c21c392d6afbb301ace
Melissa Cline <cline@cruncher.kilokluster.ucsc.edu>
2012-11-02 17:40:37 -0700

    Added the python path to the run script

--- scripts/runCgDataChecker.bash
+++ scripts/runCgDataChecker.bash
@@ -1,15 +1,14 @@
 #!/usr/bin/env bash
 
+export PYTHONPATH=/inside/home/cline/projects/cgDataAPI:$PYTONPATH
+
 freezeDir="/inside/depot/cgrepo/cgDataFreeze2012-07-11/"
 
-expDir="public/TCGA/BLCA/SNP6_genomicSegment.json"
+expDir="public/other/maser2007_public/maserCGH_genomicMatrix.json"
 scripts/cgDataChecker.py $freezeDir$expDir
 
-#expDir="public/other/maser2007_public/maserCGH_genomicMatrix.json"
-#scripts/cgDataChecker.py $freezeDir$expDir
+expDir="public/other/chin2006_public/chin2006Exp_genomicMatrix.json"
+scripts/cgDataChecker.py $freezeDir$expDir
 
-#expDir="public/other/chin2006_public/chin2006Exp_genomicMatrix.json"
-#scripts/cgDataChecker.py $freezeDir$expDir
-
-#expDir="public/other/NCI60_public/NCI60exp_genomicMatrix.json"
-#scripts/cgDataChecker.py $freezeDir$expDir
+expDir="public/other/NCI60_public/NCI60exp_genomicMatrix.json"
+scripts/cgDataChecker.py $freezeDir$expDir
